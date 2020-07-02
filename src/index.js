555187a22c499f05e32378ac76fe5f4013663154
boady <46287830+boady@users.noreply.github.com>
2020-07-02 19:54:34 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const cvs = document.getElementById("canvas");
+cvs.getContext("2d");
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
