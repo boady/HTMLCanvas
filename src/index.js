1cdeca52a5e89bad7069058bfcfdaa4e799f10fe
boady <46287830+boady@users.noreply.github.com>
2020-07-02 20:40:17 -0400

    Need images

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import "./styles.css";
 
 const cvs = document.getElementById("canvas");
-//ERROR CANNOT READY PROPERTY GETCONTEXT OF NULL
 const ctx = cvs.getContext("2d");
 let imageName = new Image();
 imageName.src - "path/img.png";
 let audioName = new Audio();
 audioName.src = "path/audio.png";
+let box = 32;
 
 ctx.drawImage(imageName, 40, 50, 25, 25);
 ctx.fillStyle = "red";
 ctx.fillRect(100, 300, 30, 30);
-function draw() {}
-
+ctx.fillStyle = "black";
+ctx.fillRect(5 * box, 6 * box, 2 * box, 3 * box);
+let snake = [];
+snake[0] = { x: 9 * box, y: 10 * box };
+function draw() {
+  ctx.drawimage(ground, 0, 0);
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i == 0 ? "green" : "white";
+    ctx.fillRect(snake[i].x, snake[i].y.box, box);
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+//let game=setinterval (draw, 100);
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
